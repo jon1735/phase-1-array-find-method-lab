5b738fe56cafbe7a444e2a8d045e9d3791d92330
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-05-25 13:53:27 -0400

    updating index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function superbowlWin(record){
+    function hasAWin (element, index, array){
+        return (element.result === `W`)
+    }
+    let superbowlWin = record.find(hasAWin)
+        console.log(superbowlWin);
 
+    if (typeof superbowlWin === "object") {
+        return superbowlWin.year
+    } else {
+        return undefined
+    }
+}
